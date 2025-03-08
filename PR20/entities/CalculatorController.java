aeb9c3f05207fff32118ac02bac7f311ce546e04
StBell <80484384+CGC-GC@users.noreply.github.com>
2025-03-09 00:38:07 +0300

    Update CalculatorController.java

--- PR20/entities/CalculatorController.java
+++ PR20/entities/CalculatorController.java
@@ -1,36 +1,103 @@
 package PR20.entities;
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
 
 public class CalculatorController {
     private CalculatorModel model;
     private CalculatorView view;
+    private double firstNumber = 0;
+    private String currentOperation = null;
+    private boolean startNewNumber = true;
 
     public CalculatorController(CalculatorModel model, CalculatorView view) {
         this.model = model;
         this.view = view;
     }
+    
     public void initController() {
         String[] buttonLabels = view.getButtonLabels();
         for (int i = 0; i < buttonLabels.length; i++) {
             view.addButtonClickListener(new ButtonClickListener(), i);
         }
     }
+    
     private class ButtonClickListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String command = ((JButton) e.getSource()).getText();
-            if (command.equals("=")) {
-                String expression = view.getInputFieldText();
-                model.appendExpression(expression);
-                double result = model.evaluateExpression();
-                view.setInputFieldText(Double.toString(result));
-                model.clearExpression();
-            } else if (command.equals("C")) { // Обрабатываем очистку строки
+            
+            // Обработка числовых кнопок
+            if (command.matches("[0-9.]")) {
+                if (startNewNumber) {
+                    view.setInputFieldText(command);
+                    startNewNumber = false;
+                } else {
+                    view.setInputFieldText(view.getInputFieldText() + command);
+                }
+            } 
+            // Обработка операций
+            else if (command.matches("[+\\-*/]")) {
+                try {
+                    // Если уже есть операция, сначала выполняем её
+                    if (currentOperation != null) {
+                        calculateResult();
+                    }
+                    
+                    firstNumber = Double.parseDouble(view.getInputFieldText());
+                    currentOperation = command;
+                    startNewNumber = true;
+                } catch (NumberFormatException ex) {
+                    view.setInputFieldText("Error");
+                    startNewNumber = true;
+                }
+            } 
+            // Обработка равно
+            else if (command.equals("=")) {
+                calculateResult();
+                currentOperation = null;
+                startNewNumber = true;
+            } 
+            // Обработка очистки
+            else if (command.equals("C")) {
                 view.setInputFieldText("");
+                firstNumber = 0;
+                currentOperation = null;
+                startNewNumber = true;
                 model.clearExpression();
-            } else {
-                view.setInputFieldText(view.getInputFieldText() + " " + command);
+            }
+        }
+        
+        private void calculateResult() {
+            try {
+                if (currentOperation != null) {
+                    double secondNumber = Double.parseDouble(view.getInputFieldText());
+                    double result = 0;
+                    
+                    switch (currentOperation) {
+                        case "+":
+                            result = firstNumber + secondNumber;
+                            break;
+                        case "-":
+                            result = firstNumber - secondNumber;
+                            break;
+                        case "*":
+                            result = firstNumber * secondNumber;
+                            break;
+                        case "/":
+                            if (secondNumber == 0) {
+                                view.setInputFieldText("Division by zero");
+                                return;
+                            }
+                            result = firstNumber / secondNumber;
+                            break;
+                    }
+                    
+                    view.setInputFieldText(String.valueOf(result));
+                    firstNumber = result;
+                }
+            } catch (NumberFormatException ex) {
+                view.setInputFieldText("Error");
+                startNewNumber = true;
             }
         }
     }
